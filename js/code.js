2bc68f223c12178a462f7dab25c1e96f37cf72de
cynthiaa <cynthiaa@live.com.ar>
2013-04-14 17:05:04 -0300

    Bug resuelto con cambio de path y agregado de vuelos.html para testeo de links

--- js/code.js
+++ js/code.js
@@ -1,7 +1,7 @@
 function init() {
 
     $("body").append("<div id='1234'></div>");
-    $("#1234").load("html/layout.html", function() {
+    $("#1234").load("/html/layout.html", function() {
 
         $("body > *").filter(":not(#1234)").appendTo("#main");
         $("#1234 > *").appendTo("body");
