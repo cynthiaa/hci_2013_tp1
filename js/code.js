63a231ed258ddeb80eaf59974cb78d58459375d8
cynthiaa <cynthiaa@live.com.ar>
2013-05-02 16:29:04 -0300

    Cambio de nombre a un id

--- js/code.js
+++ js/code.js
@@ -1,11 +1,11 @@
 function init() {
 
-    $("body").append("<div id='1234'></div>");
-    $("#1234").load("/html/layout.html", function() {
+    $("body").append("<div id='temp_div'></div>");
+    $("#temp_div").load("/html/layout.html", function() {
 
-        $("body > *").filter(":not(#1234)").appendTo("#main");
-        $("#1234 > *").appendTo("body");
-        $("#1234").remove();
+        $("body > *").filter(":not(#temp_div)").appendTo("#main");
+        $("#temp_div > *").appendTo("body");
+        $("#temp_div").remove();
     });
 }
 
