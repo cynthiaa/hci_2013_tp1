d2a374196d078131a7eeedbd6e32f134cccdd993
cynthiaa <cynthiaa@live.com.ar>
2013-05-14 05:56:54 -0300

    Template para pasajeros funcionando

--- js/passengers.js
+++ js/passengers.js
@@ -1,16 +1,26 @@
 require(
     [
         "libs/text!../templates/passengers/passengers.html",
+        "libs/text!../templates/passengers/passenger_data.html",
+        "libs/text!../templates/select.html",
         "libs/text!../templates/select.html",
         "libs/utils",
         "libs/carousel",
         "libs/domReady"
     ],
 
-    function(passengers_html, select_html) {
+    function(passengers_html, passenger_data_html, select_html) {
 
         Utils.init();
         Utils.make_non_menu_html(passengers_html);
+
+        var passenger_data_tmp = Handlebars.compile(passenger_data_html);
+
+        /* Para cada pasajero */
+
+        $('#pass-ctn').append(passenger_data_tmp({"title": "Adultos"}));
+        $('#pass-ctn').append(passenger_data_tmp({"title": "Menores"}));
+        $('#pass-ctn').append(passenger_data_tmp({"title": "Infantes"}));
 
         var select_tmp = Handlebars.compile(select_html);
 
