9a77347a2a3a320cceb0391ce16bb7ba6e9fcf16
cynthiaa <cynthiaa@live.com.ar>
2013-05-14 01:34:45 -0300

    Fecha de vencimiento en payment

--- js/payment.js
+++ js/payment.js
@@ -1,15 +1,26 @@
 require(
     [
         "libs/text!../templates/payment/payment.html",
+        "libs/text!../templates/select.html",
         "libs/utils",
         "libs/carousel",
         "libs/domReady"
     ],
 
-    function(payment_html) {
+    function(payment_html, select_html) {
 
         Utils.init();
         Utils.make_non_menu_html(payment_html);
+
+        var select_tmp = Handlebars.compile(select_html);
+
+        for (var i = 1; i <= 12; i++) {
+            $("#select_expiration_month").append(select_tmp({"value": i, "name": i}));
+        }
+
+        for (var i = 2013; i <= 2023; i++) {
+            $("#select_expiration_year").append(select_tmp({"value": i, "name": i}));
+        }
     }
 );
 
