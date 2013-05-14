2588fb0435d2a07c63903a90828fd977a4ec0f72
cynthiaa <cynthiaa@live.com.ar>
2013-05-14 02:00:52 -0300

    Fecha de nacimiento en passengers

--- js/about.js
+++ js/about.js
@@ -10,12 +10,6 @@ require(
 
         Utils.init();
         Utils.make_html(about_html);
-
-        var select_tmp = Handlebars.compile(select_html);
-
-        for (var i = 1; i <= 12; i++) {
-            $("#select_expiration_month").append(select_tmp({"value": i, "name": i}));
-        }
     }
 );
 
--- js/passengers.js
+++ js/passengers.js
@@ -1,15 +1,33 @@
 require(
     [
         "libs/text!../templates/passengers/passengers.html",
+        "libs/text!../templates/select.html",
         "libs/utils",
         "libs/carousel",
         "libs/domReady"
     ],
 
-    function(passengers_html) {
+    function(passengers_html, select_html) {
 
         Utils.init();
         Utils.make_non_menu_html(passengers_html);
+
+        var select_tmp = Handlebars.compile(select_html);
+
+        current_year = new Date().getFullYear();
+
+        for (var i = 1; i <= 31; i++) {
+            $("#select_birth_day").append(select_tmp({"value": i, "name": i}));
+        }
+
+       for (var i = 1; i <= 12; i++) {
+            $("#select_birth_month").append(select_tmp({"value": i, "name": i}));
+        }
+
+        for (var i = 1900; i <= current_year; i++) {
+            $("#select_birth_year").append(select_tmp({"value": i, "name": i}));
+        }
+
     }
 );
 
