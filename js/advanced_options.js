a3f5192a25cc61169ec8eb7f093012142b037f0d
cynthiaa <cynthiaa@live.com.ar>
2013-05-19 19:57:41 -0300

    Pequeños cambios en advanced options

--- js/advanced_options.js
+++ js/advanced_options.js
@@ -18,11 +18,13 @@ require(
         var select_tmp = Handlebars.compile(select_html);
 
         for (var i = 0; i <= 24; i++) {
+
             $("#select_departure_time").append(select_tmp({"value": i, "name": i + ":00"}));
             $("#select_return_time").append(select_tmp({"value": i, "name": i + ":00"}));
         }
 
         for (var i = 1; i <= 8; i++) {
+
             $("#select_stopovers").append(select_tmp({"value": i, "name": "Hasta " + i + " escala" + (i > 1 ? "s" : "")}));
         }
 
@@ -30,7 +32,7 @@ require(
 
         var airlines = new Array();
 
-        api.misc.getAirlines({
+        var callbacks = {
 
             success: function(result) {
 
@@ -38,24 +40,31 @@ require(
 
                 airlines[i] = result.airlines[i].name + " (" + result.airlines[i].airlineId + ")";
             }
-        }});
+        }};
+
+        api.misc.getAirlines(callbacks);
 
         $("#airline").autocomplete({
             source: airlines
         });
 
-        var callbacks = {
+        callbacks = {
 
             success: function(result) {
 
             for (var i = 0; i < result.currencies.length; i++) {
 
-                $("#currency").append(select_tmp({"value": result.currencies[i].description, "name": result.currencies[i].description}));
+                var name = result.currencies[i].description;
+
+                name = (name == "Pesos - Argentina") ? "Pesos" : name;
+
+                $("#currency").append(select_tmp({"value": name, "name": name}));
             }
         }
         };
 
         var param = {"sort_key": "id", "sort_order": "asc"};
+
         api.misc.getCurrencies(callbacks, param);
 
     }
