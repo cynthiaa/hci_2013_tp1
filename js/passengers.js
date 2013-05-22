947645a9b1c7e56721ae99a320bf532a2e64118c
cynthiaa <cynthiaa@live.com.ar>
2013-05-22 12:51:58 -0300

    Cambios en passengers para formularios más dinámicos

--- js/passengers.js
+++ js/passengers.js
@@ -1,8 +1,7 @@
 require([
         "libs/text!../templates/passengers/passengers.html",
         "libs/text!../templates/passengers/passenger_data.html",
-        "libs/text!../templates/select.html",
-        "libs/text!../templates/select.html",
+        "libs/text!../templates/passengers/passenger_title.html",
         "libs/utils",
         "libs/jquery.validate",
         "libs/jquery.maskedinput",
@@ -10,34 +9,32 @@ require([
         "libs/domReady"
         ],
 
-        function(passengers_html, passenger_data_html, select_html) {
+        function(passengers_html, passenger_data_html, passenger_title_html) {
 
             Utils.init();
             Utils.make_non_menu_html(passengers_html);
 
             var passenger_data_tmp = Handlebars.compile(passenger_data_html);
+            var passenger_title_tmp = Handlebars.compile(passenger_title_html);
 
-            /* Para cada pasajero */
+            /* Passengers */
 
-            $('#pass-ctn').append(passenger_data_tmp({
-                "title" : "Adultos"
-            }));
-            $('#pass-ctn').append(passenger_data_tmp({
-                "title" : "Menores"
-            }));
-            $('#pass-ctn').append(passenger_data_tmp({
-                "title" : "Infantes"
-            }));
-
-            var select_tmp = Handlebars.compile(select_html);
+            addPassengers("Adultos", 3);
+            addPassengers("Menores", 2);
+            addPassengers("Infantes", 1);
 
             current_year = new Date().getFullYear();
 
             Utils.dateMask("input.birth");
 
-            $("#continuar").click(function(){
-                if(validator.valid())
-                    console.log="jojoooojo";
-            });
+
+            function addPassengers(title, n) {
+
+                ($('#pass-ctn').append(passenger_title_tmp({"title": title}))).append(passenger_data_tmp({}));
+
+                while(--n) {
+                    ($('#pass-ctn').append(passenger_data_tmp({})));
+                }
+            }
 });
 
