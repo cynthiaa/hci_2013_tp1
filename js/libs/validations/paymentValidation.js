2b18b63e652ac8923d44fcce8b68a68b0309d9e1
Gabriel Zanzotti <gabriel__92@hotmail.com>
2013-05-23 04:40:33 -0300

    agregado el jsde lavalidacion de payment

---
+++ js/libs/validations/paymentValidation.js
@@ -0,0 +1,35 @@
+var validator = new FormValidator('payment-form', [{
+    name: 'card_num',
+    display: 'numero de tarjeta',    
+    rules: 'required|max_legth(16)|numeric'
+}, {
+    name: 'exp-date',
+    display: 'fecha de vencimiento'
+    rules: 'required|max_length(10)'
+}, {
+    name: 'security_code',
+    display: 'codigo de seguridad',
+    rules: 'required|max_length(3)|numeric'
+}, {
+    name: 'name',
+    display: 'nombre',
+    rules: 'required|alpha'
+}, {
+    name: 'lastname',
+    display: 'apellido',
+    rules: 'required|alpha'
+}, {
+    name: 'dni',
+    rules: 'required|numeric|max_length(8)'
+}, {
+    name: 'email',
+    rules: 'required|valid_email'
+}], function(errors, event) {
+	if (errors.length > 0) {
+		var errorString = '';
+		for (var i = 0, errorLength = errors.length; i < errorLength; i++) {
+			errorString += errors[i].message + '<br />';
+			console.log(errors[i].message);
+		}
+	} 
+});
