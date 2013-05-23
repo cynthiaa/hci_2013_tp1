91d30932c44d74fa26b1eafa7d33edcee3691b0e
Gabriel Zanzotti <gabriel__92@hotmail.com>
2013-05-23 03:30:07 -0300

    tamaño op avanzadas fixeado y agregue 2 valid js

---
+++ js/libs/validations/contactValidation.js
@@ -0,0 +1,20 @@
+var validator = new FormValidator('contact-form', [{
+	name: 'subject',
+	display: 'asunto',    
+	rules: ''
+	}, {
+	name: 'email',
+	rules: 'valid_email|required'
+	}, {
+	name: 'body',
+	display:'mensaje',
+	rules: ''
+	}], function(errors, event) {
+	if (errors.length > 0) {
+		var errorString = '';
+		for (var i = 0, errorLength = errors.length; i < errorLength; i++) {
+			errorString += errors[i].message + '<br />';
+			console.log(errors[i].message);
+		}
+	}  
+});	
---
+++ js/libs/validations/sideValidation.js
@@ -0,0 +1,27 @@
+var validate = new function() {
+	var validator = new FormValidator('side-form', [{
+		name: 'from',
+		display: 'origen',    
+		rules: 'required'
+		}, {
+		name: 'to',
+		display: 'destino',
+		rules: 'required'
+		}, {
+		name: 'depart',
+		display:'partida',
+		rules: 'required'
+		},{
+		name: 'return',
+		display: 'regreso',
+		rules: 'required'
+		}], function(errors, event) {
+		if (errors.length > 0) {
+			var errorString = '';
+			for (var i = 0, errorLength = errors.length; i < errorLength; i++) {
+				errorString += errors[i].message + '<br />';
+				console.log(errors[i].message);
+			}
+		}  
+	});	
+}
