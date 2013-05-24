3486c01556b19e3308c956e02210e03093d71af2
Germán Rodrigo Romarión <grromarion@hotmail.com>
2013-05-24 20:11:31 -0300

    agregando payment validations...

--- js/libs/validations/paymentValidation.js
+++ js/libs/validations/paymentValidation.js
@@ -1,4 +1,4 @@
-var validator = new FormValidator('payment-form', [{
+var validator = new formvalidator('payment-form', [{
     name: 'card_num',
     display: 'numero de tarjeta',    
     rules: 'required|max_legth(16)|numeric'
@@ -26,13 +26,29 @@ var validator = new FormValidator('payment-form', [{
     rules: 'required|valid_email'
 }], function(errors, event) {
 	if (errors.length > 0) {
-		var errorString = '';
-		for (var i = 0, errorLength = errors.length; i < errorLength; i++) {
-			errorString += errors[i].message + '<br />';
-			console.log(errors[i].message);
+		var selector_errors = $('.payment-error');
+		for (var i = 0, errorlength = errors.length; i < errorlength; i++) {
+			selector_errors.append(errors[i].message + '<br />');
 		}
+		selector_errors.fadein(200);
 	} else {
-		Utils.stopEvent(e);
-		window.location.href = Utils.getUrl("confirmation.html", Utils.setAttrs());
+		utils.stopevent(e);
+		/*
+		var param = {
+                "number": $('#card-num').val(),
+                "exp_date": utils.convertexpirationdate($('#select_expiration_month').val(), $('#select_expiration_year').val()),
+                "sec_code": $('#security-code').val()
+		};
+
+		var callback = {
+			success: function(result) {
+				window.alert("los datos de la tarjeta son " + (result.valid == true ? "" : "in") + "correctos");
+			}
+		}
+
+		console.log(param);
+		api.booking.validatecreditcard(callback, param);
+           */ 
+		window.location.href = utils.geturl("confirmation.html", utils.setattrs());
 	}
 });
