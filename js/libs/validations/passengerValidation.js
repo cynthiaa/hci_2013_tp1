79f56f9aee1f0a3afcad11fe2cb67031c44a89d1
Tomas Lori <tomas_lori142@hotmail.com>
2013-05-27 02:48:23 -0300

    validation update
    banda de escupida

--- js/libs/validations/passengerValidation.js
+++ js/libs/validations/passengerValidation.js
@@ -1,16 +1,16 @@
 console.log("ESTOY EN EL VALIDADOR!");
-var passengerRules = [];
+var passengerRules = createPassengerRules();
 var MAX_PASSENGERS = 7;
-var validator = new FormValidator('passenger-form', createPassengerRules(passengerRules), function(errors, e) {
+var validator = new FormValidator('passenger-form', passengerRules, function(errors, e) {
 	console.log("ESTOY EN LA FUNCION!");
 	var SELECTOR_ERRORS = $('.passenger-error');
 	if (errors.length > 0) {
 		console.log("IF!");
 		SELECTOR_ERRORS.empty();
 
-		for (var i = 0, errorLength = errors.length; i < errorLength; i++) {
+		for (var i = 0, errorLength = errors.length; i < errorLength; i++)
 			SELECTOR_ERRORS.append(errors[i].message + '<br />');
-		}
+			
 		SELECTOR_ERRORS.fadeIn(200);
 	} else {
 		console.log("ELSE!");
@@ -19,33 +19,26 @@ var validator = new FormValidator('passenger-form', createPassengerRules(passeng
 	}
 });
 
-var createPassengerRules = new function(passengerRules) {
+var createPassengerRules = new function() {
 	console.log("CREATE PASSENGER RULES!");
+	var passengerRules = new Array();
+	
 	for (var i = 0; i < MAX_PASSENGERS; i++) {
 	  passengerRules[i] = {
 		name : 'name' + i,
 		display : 'Nombre',
 		rules : 'required|alpha'
 		}
-	};
-	
-	for (var i = 0; i < MAX_PASSENGERS; i++) {
 	  passengerRules[MAX_PASSENGERS + i] = {
 		name : 'lastname' + i,
 		display : 'Apellido',
 		rules : 'required|alpha'
 		}
-	};
-	
-	for (var i = 0; i < MAX_PASSENGERS; i++) {
 	  passengerRules[MAX_PASSENGERS*2 + i] = {
 		name : 'birth' + i,
 		display : 'Fecha de nacimiento',
 		rules : 'required|alpha'
 		}
-	};
-	
-	for (var i = 0; i < MAX_PASSENGERS; i++) {
 	  passengerRules[MAX_PASSENGERSS*3 + i] = {
 		name : 'gender' + i,
 		display : 'Sexo',
